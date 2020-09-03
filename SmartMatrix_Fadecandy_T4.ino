44bdddb06db4249b7f17a6751a3254395d35392b
Louis Beaudoin <louis@embedded-creations.com>
2020-09-03 08:09:50 +0100

    Configure for 128x64, simplify use of bitmaps

--- SmartMatrix_Fadecandy_T4.ino
+++ SmartMatrix_Fadecandy_T4.ino
@@ -12,35 +12,26 @@
 //#include <MatrixHardware_KitV1.h>       // SmartMatrix Shield for Teensy 3 V1-V3
 //#include <MatrixHardware_KitV4.h>       // SmartLED Shield for Teensy 3 V4
 //#include <MatrixHardware_KitV4T4.h>     // Teensy 4 Wired to SmartLED Shield for Teensy 3 V4
-//#include <MatrixHardware_T4Adapter.h>   // Teensy 4 Adapter attached to SmartLED Shield for Teensy 3 V4
+#include <MatrixHardware_T4Adapter.h>   // Teensy 4 Adapter attached to SmartLED Shield for Teensy 3 V4
 //#include "MatrixHardware_Custom.h"      // Copy an existing MatrixHardware file to your Sketch directory, rename, customize, and you can include it like this
 #include <SmartMatrix3.h>
 
 #include "gimpbitmap.h"
 
-// pixelmatix.c is a bitmap exported from GIMP without modification
-#include "pixelmatix.c"
-
 // colorwheel.c has been modified to use the gimp32x32bitmap struct
 #include "colorwheel.c"
 
-// chrome16 is a 16x16 pixel bitmap, exported from GIMP without modification
-#include "chrome16.c"
-
 #define COLOR_DEPTH 24                  // known working: 24, 48 - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24
-const uint16_t kMatrixWidth = 32;        // known working: 32, 64, 96, 128, 256
-const uint16_t kMatrixHeight = 32;       // known working: 16, 32, 48, 64, 128
+const uint16_t kMatrixWidth = 128;        // known working: 32, 64, 96, 128, 256
+const uint16_t kMatrixHeight = 64;       // known working: 16, 32, 48, 64, 128
 const uint8_t kRefreshDepth = 36;       // known working: 24, 36, 48 (on Teensy 4.x: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48)
 const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save memory, more to keep from dropping frames and automatically lowering refresh rate
-const uint8_t kPanelType = SMARTMATRIX_HUB75_32ROW_MOD16SCAN;   // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels
+const uint8_t kPanelType = SMARTMATRIX_HUB75_64ROW_MOD32SCAN;   // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels
 const uint8_t kMatrixOptions = (SMARTMATRIX_OPTIONS_NONE);      // see http://docs.pixelmatix.com/SmartMatrix for options
 const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
 
 SMARTMATRIX_ALLOCATE_BUFFERS(matrix, kMatrixWidth, kMatrixHeight, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
 SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kBackgroundLayerOptions);
-
-int led = -1;   // Set to -1 to disable LED flash, only needed for debugging purposes
-//int led = 13; // builtin LED pin on the Teensy, interferes with refresh on Teensy 4
 
 void drawBitmap(int16_t x, int16_t y, const gimp32x32bitmap* bitmap) {
   for(unsigned int i=0; i < bitmap->height; i++) {
@@ -65,43 +56,15 @@ void setup() {
 
   matrix.setBrightness(128);
 
-  if(led >= 0)  pinMode(led, OUTPUT);
-}
-
-void loop() {
   int x, y;
-  backgroundLayer.fillScreen({0,0,0});
-  x = (kMatrixWidth / 2) - (pixelmatixlogo.width/2);
-  y = (kMatrixHeight / 2) - (pixelmatixlogo.height/2);
-  // to use drawBitmap, must cast the pointer to pixelmatixlogo as (const gimp32x32bitmap*)
-  drawBitmap(x,y,(const gimp32x32bitmap*)&pixelmatixlogo);
-  backgroundLayer.swapBuffers();
-
-  if(led >= 0)  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);
-  if(led >= 0)  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);
-
   backgroundLayer.fillScreen({0,0,0});
   x = (kMatrixWidth / 2) - (colorwheel.width/2);
   y = (kMatrixHeight / 2) - (colorwheel.height/2);
   // can pass &colorwheel in directly as the bitmap source is already gimp32x32bitmap
   drawBitmap(x,y,&colorwheel);
   backgroundLayer.swapBuffers();
+}
 
-  if(led >= 0)  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);
-  if(led >= 0)  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);
+void loop() {
 
-  backgroundLayer.fillScreen({0,0,0});
-  x = (kMatrixWidth / 2) - (chrome16.width/2);
-  y = (kMatrixHeight / 2) - (chrome16.height/2);
-  drawBitmap(x, y, (const gimp32x32bitmap*)&chrome16);
-  backgroundLayer.swapBuffers();
-
-  if(led >= 0)  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);
-  if(led >= 0)  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);
 }
